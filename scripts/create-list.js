2565de9000660c0543e42b1446edab344929ef0b
Dipam Sen <dipamdiptam@gmail.com>
2023-09-27 12:03:43 +0530

    create workflow

---
+++ scripts/create-list.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+const readme = fs.readFileSync(path.join(__dirname, "..", "README.md"), "utf8");
+
+let front = readme.slice(0, readme.indexOf("<!-- BEGIN-AUTO-LIST -->"));
+
+const contributions = fs
+  .readdirSync(path.join(__dirname, "..", "contributions"))
+  .filter((file) => file.endsWith(".json"))
+  .map((file) => require(path.join(__dirname, "..", "contributions", file)));
+
+const list = contributions
+  .map(
+    (contribution) => `### [${contribution.title}](${contribution.url})
+
+${contribution.description}`
+  )
+  .join("\n\n");
+
+const newReadme = `${front}<!-- BEGIN-AUTO-LIST -->
+
+${list}
+
+<!-- END-AUTO-LIST -->`;
+
+console.log(newReadme);
